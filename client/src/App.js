e3a98f69c153417d1e50ee2831749896d2177cd2
Lakshman155 <vvlakshmankandula44@gmail.com>
2024-01-24 21:44:49 +0530

    Navbar added

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import {Routes,Route} from 'react-router-dom'
+import Signup from './components/auth/Signup';
+import Login from './components/auth/Login';
+import Navbar from './components/headers/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Navbar/>
+
+    <Routes>
+      <Route to='/signup' element={<Signup/>}></Route>
+      <Route to='/login' element={<Login/>}></Route>
+    </Routes>
+    
+     
     </div>
   );
 }
