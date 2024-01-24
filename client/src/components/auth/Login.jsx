21fc152c2d73735a2efd14947ce82953fdd1437a
Lakshman155 <vvlakshmankandula44@gmail.com>
2024-01-24 09:15:28 +0530

    added login

---
+++ client/src/components/auth/Login.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <div>
+    <form>
+
+        <input type='text' name ='username' placeholder='username...' required>
+
+        </input>
+    </form>
+      
+    </div>
+  )
+}
+
+export default Login
