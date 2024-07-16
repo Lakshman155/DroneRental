ac7eb2108c1afe422f4772364d859145b777ba4b
Lakshman155 <vvlakshmankandula44@gmail.com>
2024-07-16 19:36:09 +0530

    added the button in product card

--- client/src/components/product/Productcard.jsx
+++ client/src/components/product/Productcard.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import api from '../api/Api';
+import Button from '@mui/material/Button';
+// import 'tailwindcss/tailwind.css'
 
 const ProductCard = ({ product}) => {
 
@@ -8,7 +10,7 @@ const ProductCard = ({ product}) => {
 
 
     try{
-      await api.post('/addrentals',{product})
+      await api.post('/rent/addrentals',{product})
       alert('added your rental')
 
     }
@@ -27,10 +29,15 @@ const ProductCard = ({ product}) => {
         <div className="font-bold text-xl mb-2">{product.name}</div>
         <p className="text-gray-700 text-base">{product.description}</p>
         <p className="text-gray-900 font-bold mt-2">${product.price}/day</p>
+
+
+
       </div>
 
+      
 
-      <button onClick={addtorent}>rent</button>
+
+      <Button variant='contained'  onClick={addtorent}>rent</Button>
     </div>
   );
 };
